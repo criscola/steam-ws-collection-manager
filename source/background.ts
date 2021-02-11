4adee2edf29c3f7a69d08863b9f1edf32392b588
criscola <cristiano.colangelo@bluewin.ch>
2021-02-11 15:50:22 +0100

    Add subscription to full collections

--- source/background.ts
+++ source/background.ts
@@ -1,5 +1,9 @@
-import Cookie = browser.cookies.Cookie;
+import { Config, Collection } from "./options-storage";
+
 class SteamApi {
+	static readonly subscribeUri = "https://steamcommunity.com/sharedfiles/subscribe";
+	static readonly addToCollectionUri = "https://steamcommunity.com/sharedfiles/ajaxaddtocollections"
+
 	static async SubscribeToSingleItem(id: string): Promise<Response> {
 		const sessionId = await this.getSessionId();
 		if (sessionId === null) {
@@ -7,15 +11,44 @@ class SteamApi {
 			throw new Error("sessionid cannot be null");
 		}
 
-		return fetch("https://steamcommunity.com/sharedfiles/subscribe", {
-			method: 'POST',
+		return fetch(this.subscribeUri, {
+			method: "POST",
 			headers: this.getBaseHeaders(),
 			body: new URLSearchParams({
-				'id': id,
-				'appid': this.getAppId(),
-				'sessionid': sessionId
+				"id": id,
+				"appid": this.getAppId(),
+				"sessionid": sessionId
 			}),
-			redirect: 'follow'
+			redirect: "follow"
+		});
+	}
+
+	static async AddItemToFullCollection(itemId: string): Promise<Response> {
+		console.log("Adding item to full collection...");
+		const sessionId = await this.getSessionId();
+		if (sessionId === null) {
+			console.error("could not find sessionid in cookies");
+			throw new Error("sessionid cannot be null");
+		}
+
+		const fullCollection = await this.getNextFullCollection(Config.fullCollections);
+		if (fullCollection === null) {
+			console.error("could not find the next full collection");
+			throw new Error("fullCollection cannot be null");
+		}
+		const itemShouldBeAddedKey = "collections["+fullCollection.id+"][add]";
+		const collectionTitleKey = "collections["+fullCollection.id+"][title]";
+
+		return fetch(this.addToCollectionUri, {
+			method: "POST",
+			headers: this.getBaseHeaders(),
+			body: new URLSearchParams({
+				"sessionID": sessionId,
+				"publishedfileid": itemId,
+				[itemShouldBeAddedKey]: "true",
+				[collectionTitleKey]: fullCollection.title,
+			}),
+			redirect: "follow"
 		});
 	}
 
@@ -51,6 +84,56 @@ class SteamApi {
 		console.log(cookies);
 		return cookies.find(i => i.name === "sessionid")?.value ?? null;
 	}
+
+	// Returns the next available full collection. "Next available" in this context means the first full collection
+	// which can hold at least one new item. E.g. there are 3 full collections, 'a', 'b' and 'c'. 'a' contains 1000
+	// items thus is at full capacity. 'b' contains 999 elements, consequently 'b' is returned as it's the first
+	// available collection from the list. If no available collections are present, null is returned.
+	// TODO: Imagine having 200 items to add, requesting 200 times if an item can be added to a collection will generate 200 HTTP requests --> very undesirable.
+	// TODO: Refactor to return count of items for each full collection and let the caller handle it.
+	private static async getNextFullCollection(configKey: string): Promise<Collection | null>  {
+		const result = await browser.storage.sync.get(configKey);
+		if (result[configKey].length < 1) {
+			console.error("could not find any full collection in config");
+			return null;
+		}
+		for (let elem of result[configKey]) {
+			const collection = await this.parseCollectionPage(elem);
+			if (collection.count <= 1000) {
+				return collection;
+			}
+		}
+		return null;
+	}
+
+	private static async parseCollectionPage(collectionId: string): Promise<Collection> {
+		const sessionId = await this.getSessionId();
+		if (sessionId === null) {
+			console.error("could not find sessionid in cookies");
+			throw new Error("sessionid cannot be null");
+		}
+
+		const response = await fetch("https://steamcommunity.com/sharedfiles/filedetails/?id="+collectionId, {
+			method: "GET",
+			headers: this.getBaseHeaders(),
+		});
+
+		const body = await response.text();
+		const html = new DOMParser().parseFromString(body, 'text/html');
+
+		const title = html.querySelector(".collectionHeaderContent")?.querySelector(".workshopItemTitle")?.textContent;
+		if (title === null || title === undefined) {
+			console.error("could not find title in collection page");
+			throw new Error("title cannot be null");
+		}
+		const itemsCountElement = html.querySelector(".workshopItemDescriptionTitle")?.querySelector(".childCount")?.textContent;
+		if (itemsCountElement === null || itemsCountElement === undefined) {
+			console.error("could not find itemsCount in collection page");
+			throw new Error("itemsCountElement cannot be null");
+		}
+		const itemsCount = Number(itemsCountElement?.substring(1, itemsCountElement.length - 1));
+		return new Collection(collectionId, title, itemsCount);
+	}
 }
 
 async function getCookies() {
@@ -65,8 +148,11 @@ function handleMessage(request, _, sendResponse) {
 	SteamApi.SubscribeToSingleItem(request.msg[0].id)
 		.then(response => response.text())
 		.then(result => console.log(result))
-		.then(() => sendResponse("Item subscribed!"))
-		.catch(error => console.log('error', error));
+		.then(() => SteamApi.AddItemToFullCollection(request.msg[0].id))
+		.then(response => response.text())
+		.then(result => console.log(result))
+		.then(() => sendResponse("item subscribed!"))
+		.catch(error => console.log("error while subscribing to item", error));
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
